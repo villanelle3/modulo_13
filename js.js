778712bf4ffb58681fa88a1b91373c6c8297928a
Laura Augusta <laurah.algusta@gmail.com>
2023-02-05 14:43:12 -0300

    making it responsive via js

--- js.js
+++ js.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
-
-    $("form").on("submit", function(e){
-        e.preventDefault();
-        $('form').trigger("reset");
+    $(window).resize(function(){
+        var width = $(window).width();
+        if(width <= 959 && width >= 768 ){
+            $('.item').removeClass('col-md-4').addClass('col-md-6');
+        }
+        else{
+            $('.item').removeClass('col-md-6').addClass('col-md-4');
+        }
     })
 });
 
